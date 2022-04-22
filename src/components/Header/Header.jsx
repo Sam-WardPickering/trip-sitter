2cbe4548e77ff3ad077003afdf7a44c6e17dd3c1
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-04-22 13:10:50 +1200

    Create header component // add MUI styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 
 function Header() {
   return (
-    <h1>Header</h1>
-  )
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Trip Sitter
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
+          </Typography>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
-export default Header
+export default Header;
