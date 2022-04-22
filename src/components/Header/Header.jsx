9f9470c35c13957be1c930f09f25f670d46b5bfe
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-04-22 13:29:37 +1200

    Link MUI stylesheet

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
+import useStyles from './styles'
 
 function Header() {
+  const classes = useStyles();
+
   return (
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
@@ -13,6 +18,14 @@ function Header() {
           <Typography variant="h6" className={classes.title}>
             Explore new places
           </Typography>
+          {/* <Autocomplete> */}
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder="Search ... " classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+            </div>
+          {/* </Autocomplete> */}
         </Box>
       </Toolbar>
     </AppBar>
