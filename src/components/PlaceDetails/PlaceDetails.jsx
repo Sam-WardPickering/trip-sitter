d6fca49004a024165f26900dbd12878fb7d3ad23
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-04-26 14:31:23 +1200

    Add buttons for website and trip advisor

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -46,6 +46,14 @@ function PlaceDetails({ place }) {
             <PhoneIcon /> {place.phone}
           </Typography>
         )}
+        <CardActions>
+          <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')}>
+            Trip Advisor
+          </Button>
+          <Button size="small" color="primary" onClick={() => window.open(place.website, '_blank')}>
+            Website
+          </Button>
+        </CardActions>
       </CardContent>
 
     </Card>
