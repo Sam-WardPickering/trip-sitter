4d6618d14cea7e073f8a87c353157c3495e7f943
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-04-26 13:27:54 +1200

    Display nearby restaurants based on geolocation

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core'
 
 function PlaceDetails({ place }) {
+  console.log(place)
   return (
     <h1>{place.name}</h1>
   )
