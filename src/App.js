06471dd3958f834b383c89152ec78a6821390968
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-04-27 18:29:06 +1200

    Display hotels

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ const App = () => {
   const [coordinates, setCoordinates] = useState({});
   const [bounds, setBounds] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(( { coords: { latitude, longitude } }) => {
@@ -22,12 +24,12 @@ const App = () => {
   useEffect(() => {
     setIsLoading(true)
 
-    getPlacesData(bounds.sw, bounds.ne)
+    getPlacesData(type, bounds.sw, bounds.ne)
       .then((data) => {
         setPlaces(data);
         setIsLoading(false);
       })
-  }, [coordinates, bounds]);
+  }, [type, coordinates, bounds]);
 
   return (
     <>
@@ -35,7 +37,15 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
-          <List places={places} childClicked={childClicked} isLoading={isLoading}/>
+          <List 
+            places={places} 
+            childClicked={childClicked} 
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map 
